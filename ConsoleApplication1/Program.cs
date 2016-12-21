d2772505f3f5705bc8b81cd21d15da5cf0234283
Sergi <Sergi@DAM2>
2016-12-21 21:02:04 +0100

    Canvi

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             for (int i = 0; i < 10; i++) {
-                Console.WriteLine("hola");
+                Console.WriteLine("hola mon");
             }
         }
     }
